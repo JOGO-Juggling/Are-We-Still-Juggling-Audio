3be9bd5b6b98d20bcc2106c83873b5ca4a79b566
Sam Titarsolej <s.titarsolej@gmail.com>
2020-08-26 15:16:22 +0200

    EPD update

--- EPD.py
+++ EPD.py
@@ -6,11 +6,11 @@ from scipy import fftpack
 import math
 
 class AudioReader: 
-    def __init__(self, data_path, window_size, interval):
+    def __init__(self, data_path, frame_size, frame_stride):
         self.samplerate, self.data = wavfile.read(data_path)
         self.length = self.data.shape[0]
-        self.window_size = int((self.samplerate / 1000) * window_size)
-        self.interval = int((self.samplerate / 1000) * interval)
+        self.frame_size = int((self.samplerate / 1000) * frame_size)
+        self.frame_stride = int((self.samplerate / 1000) * frame_stride)
 
     def __iter__(self):
         # Create iterator
@@ -20,49 +20,44 @@ class AudioReader:
     def __next__(self):
         # Loop over frames
         if self.cur_sample < self.length:
-            result = self.data[self.cur_sample:self.cur_sample + self.window_size]
-            self.cur_sample += self.interval
+            result = self.data[self.cur_sample:self.cur_sample + self.frame_size]
+            self.cur_sample += self.frame_stride
             return result
         else:
             raise StopIteration
 
 def main(audio_path):
-    audioreader = AudioReader(audio_path, 150, 50)
-    peak_variance = 100
+    frame_size, frame_stride = 30, 10
+    audioreader = AudioReader(audio_path, frame_size, frame_stride)
+    wav_output = []
 
-    ste, epd, epd_val, avg_peak = [0], [], [], []
+    ste, ste_time, epd, epd_time = [], [], [], []
+    prev_ste0, prev_ste1 = 0, 0
 
     for i, samples in enumerate(audioreader):
         cur_ste = np.mean(np.abs(samples))
-        angle = math.degrees(math.atan(cur_ste - ste[-1]))
 
-        peak_avg = np.mean(avg_peak)
-        within_avg = cur_ste > peak_avg - peak_variance and cur_ste < peak_avg + peak_variance and peak_avg
+        # if i * frame_stride > 1000:
+        wav_output += list(samples)
+        if prev_ste1 > prev_ste0 and prev_ste1 > cur_ste:
+            if abs(prev_ste0 - prev_ste1) > 5 and cur_ste > 400:
+                epd.append(prev_ste1)
+                epd_time.append((i - 1) * frame_stride)
 
-        if len(avg_peak) < 1:
-            within_avg = True
-
-        if i > 50:
-            prev_epd = epd[-1] if len(epd) > 0 else -10
-            if angle > 80 and within_avg and i - prev_epd > 4:
-                epd.append(i)
-                epd_val.append(cur_ste)
-                avg_peak.append(cur_ste)
-                # if len(avg_peak) > 10:
-                #     avg_peak.pop(0)
         ste.append(cur_ste)
+        ste_time.append(i * frame_stride)
 
-        if i > 500:
-            break
+        prev_ste0 = prev_ste1
+        prev_ste1 = cur_ste
+
+        # if i * frame_stride > 35000:
+        #     break
     
-    ste = np.array(ste)
-    plt.plot(np.arange(len(ste)), ste)
-    plt.hlines(np.mean(avg_peak), xmin=0, xmax=len(ste), linestyle='--', color='m')
-    plt.hlines(np.mean(avg_peak) + peak_variance, xmin=0, xmax=len(ste), linestyle='-.', color='g')
-    plt.hlines(np.mean(avg_peak) - peak_variance, xmin=0, xmax=len(ste), linestyle='-.', color='g')
-    plt.scatter(epd, epd_val, color='r')
-
+    print(len(epd))
+    wavfile.write('out.wav', audioreader.samplerate * 3, np.array(wav_output))
+    plt.plot(ste_time, ste)
+    plt.scatter(epd_time, epd, color='r')
     plt.show()
 
 if __name__ == '__main__':
-    main('data/audio/IMG_0786.wav')
+    main('data/audio/7fc0c7ecc2394668a9b43e6724a46770.wav')
