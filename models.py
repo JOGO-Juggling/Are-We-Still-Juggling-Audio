7f48f5bb3cb46c5eef5a4e6f7bb5a806ac1c500b
Sam Titarsolej <s.titarsolej@gmail.com>
2020-08-29 14:43:45 +0200

    model v1

---
+++ models.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LogisticRegressor(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(LogisticRegressor, self).__init__()
+        self.layer = nn.Linear(in_dim, out_dim)
+
+    def forward(self, x):
+        return self.layer(x)
