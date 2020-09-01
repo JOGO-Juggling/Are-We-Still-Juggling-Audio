ef6a1379d8b8201f3b8736480e25f9d033c67a62
Sam Titarsolej <s.titarsolej@gmail.com>
2020-09-01 20:20:27 +0200

    models update

--- models.py
+++ models.py
@@ -7,9 +7,7 @@ class LogisticRegressor(nn.Module):
         super(LogisticRegressor, self).__init__()
 
         self.main = nn.Sequential(
-            nn.Linear(in_dim, 20),
-            nn.Linear(20, 20),
-            nn.Linear(20, out_dim)
+            nn.Linear(in_dim, out_dim)
         )
 
     def forward(self, x):
